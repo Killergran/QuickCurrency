db8f617a1ce09d0428e52c19a1f7813c927d871c
Jakop Kilegran <Killergran@gmail.com>
2024-12-06 21:20:47 +0100

    Add Community Toolkit for MAUI

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
+using CommunityToolkit.Maui;
+
 
 namespace QuickCurrency
 {
@@ -9,6 +11,7 @@ namespace QuickCurrency
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
